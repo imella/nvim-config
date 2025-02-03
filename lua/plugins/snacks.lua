43e75dfef56a820ff7d359d1f7d6213416a702a6
Ignacio Mella <imellat@gmail.com>
2025-02-03 15:49:57 -0300

    add snacks plugin

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,51 @@
+return {
+	"folke/snacks.nvim",
+	dependencies = {
+		"echasnovski/mini.icons",
+	},
+	priority = 1000,
+	lazy = false,
+	opts = {
+		bigfile = { enabled = true },
+		dashboard = {
+			enabled = true,
+			preset = {
+				header = [[
+                                              
+       ████ ██████           █████      ██
+      ███████████             █████ 
+      █████████ ███████████████████ ███   ███████████
+     █████████  ███    █████████████ █████ ██████████████
+    █████████ ██████████ █████████ █████ █████ ████ █████
+  ███████████ ███    ███ █████████ █████ █████ ████ █████
+ ██████  █████████████████████ ████ █████ █████ ████ ██████
+        ]],
+			},
+		},
+		indent = { enabled = true },
+		input = { enabled = true },
+		git = { enabled = true },
+		picker = { enabled = true },
+		notifier = { enabled = true },
+		quickfile = { enabled = true },
+		scroll = { enabled = false },
+		statuscolumn = { enabled = true },
+		words = { enabled = true },
+	},
+	keys = {
+		{
+			"<leader>lg",
+			function()
+				Snacks.lazygit()
+			end,
+			desc = "Lazygit",
+		},
+		{
+			"<leader><leader>",
+			function()
+				Snacks.picker.recent()
+			end,
+			desc = "Recent Files",
+		},
+	},
+}
