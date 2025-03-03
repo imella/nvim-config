1d6cbd2106431e223626ca9c6b0fa10fac1fb7ad
Ignacio Mella <imellat@gmail.com>
2025-03-03 12:32:46 -0300

    add noice.nvim for nicer command line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,14 +1,23 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "catppuccin",
-        section_separators = { left = ' ', right = ''},
-      }
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "catppuccin",
+				section_separators = { left = " ", right = "" },
+			},
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
+			},
+		})
+	end,
 }
 
 --set -g @catppuccin_window_left_separator ""
