e532d1c5add3af11c59a53203258641c5b241f00
Ignacio Mella <imellat@gmail.com>
2025-03-21 16:53:15 -0300

    set line number color to lavender

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,12 +1,17 @@
-  return {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha"
-      })
-      vim.cmd.colorscheme "catppuccin"
-    end
-  }
+return {
+	"catppuccin/nvim",
+	lazy = false,
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "mocha",
+			custom_highlights = function(colors)
+				return {
+					LineNr = { fg = colors.lavender },
+				}
+			end,
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
+}
